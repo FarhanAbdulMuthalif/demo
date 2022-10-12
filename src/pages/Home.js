c5a829b5185c2ec6cd91a210b0aeef351f7cab36
FarhanAbdulMuthalif <102349136+FarhanAbdulMuthalif@users.noreply.github.com>
2022-10-12 17:34:37 +0530

    try for deploy

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ import "./home.css";
 function Home() {
   return (
     <div>
-      <h1>Home page</h1>
+      <h1>Home page Like you here</h1>
       <div className="home">
         <img
           src="https://media.istockphoto.com/photos/paperless-workplace-idea-esigning-electronic-signature-document-an-picture-id1349390515?b=1&k=20&m=1349390515&s=170667a&w=0&h=d9eagqJMGdGExzPlpxp1UdhHi23NJTubYzKB4zPTs_s="
